afa628141a5d48cabe9aa576b9be9c0d3a714b2a
Steb125 <71096872+Steb125@users.noreply.github.com>
2020-12-28 15:28:20 -0600

    A cool React Project

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+// basic template
+import React from 'react';
+// class name should match file name
+
+// const Header = () => {
+//     return (
+//          <div></div>
+//     );
+// }
+
+// export default Header;
+
+const Header = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Header;
